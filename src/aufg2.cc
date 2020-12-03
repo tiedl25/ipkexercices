55c1eafe6e7d2363083dc4968536e4a31f10bfb4
Max Tiedl <max.tiedl@gmx.de>
2020-12-03 11:42:41 +0100

    all tasks completed

--- src/aufg2.cc
+++ src/aufg2.cc
@@ -2,10 +2,12 @@
 #include <fstream>
 #include <vector>
 #include <array>
+#include <algorithm>
 
-std::vector<std::array<double, 2>> read_point_from_file(std::string filename)
+//Liest Punkte aus Datei und speichert sie in Vector
+std::vector<std::array<double, 2>> read_points_from_file(std::string filename)
 {
-    std::vector<std::array<double, 2>> pointVector{30};
+    std::vector<std::array<double, 2>> pointVector;
     std::fstream file;
     file.open(filename, std::ios::in);
 
@@ -17,8 +19,7 @@ std::vector<std::array<double, 2>> read_point_from_file(std::string filename)
         while(std::getline(file, line))
         {
             cache = line.find(" ");
-            std::cout << stold(line.substr(0, cache)) << "    " << stold(line.substr(cache, sizeof(line))) << std::endl;
-            pointVector[i] = {stod(line.substr(0, cache)), stod(line.substr(cache, sizeof(line)))};
+            pointVector.push_back({stod(line.substr(0, cache)), stod(line.substr(cache, sizeof(line)))});
             i++;
         }
         file.close();
@@ -27,9 +28,101 @@ std::vector<std::array<double, 2>> read_point_from_file(std::string filename)
     return pointVector;
 }
 
+void write_points_in_file(std::string filename, std::vector<std::array<double, 2>>& points)
+{
+    std::fstream file;
+    file.open(filename, std::ios::out);
+
+    for(int i=0; i<points.size(); i++)
+    {
+        file << points[i][0] << " " << points[i][1] << std::endl;
+    }
+
+    file.close();
+}
+
+bool sort_by_y(std::array<double, 2> a, std::array<double, 2> b)
+{
+    if(a[1] < b[1] || (a[1] == b[1] && a[0] < b[0]))
+        return 1;
+
+    return 0;
+}
+
+bool sort_by_angle(std::array<double, 2> a, std::array<double, 2> b)
+{
+    if(a[0]*b[1]-a[1]*b[0] > 0  || (a[0]*b[1]-a[1]*b[0] == 0 && std::abs(a[0]) > std::abs(b[0])))
+        return 1;
+
+    return 0;
+}
+
+//Überprüft ob ein Punkt Rechts oder links eines Vectors liegt
+double leftOrRight(std::vector<std::array<double, 2>>& grahamStack, std::array<double, 2>& point)
+{
+    return (grahamStack[grahamStack.size() - 1][0] - grahamStack[grahamStack.size() - 2][0])
+         * (point[1] - grahamStack[grahamStack.size() - 2][1])
+         - (point[0] - grahamStack[grahamStack.size() - 2][0])
+         * (grahamStack[grahamStack.size() - 1][1] - grahamStack[grahamStack.size() - 2][1]);
+}
+
+//Graham-Scan Algorithmus
+std::vector<std::array<double, 2>> grahamScan(std::vector<std::array<double, 2>>& points, std::vector<std::array<double, 2>>& grahamStack)
+{
+    int i = 0;
+
+    while(i<points.size())
+    {
+        if(i<2 || leftOrRight(grahamStack, points[i]) >= 0 || grahamStack.size() == 2)
+        {
+            grahamStack.push_back(points[i]); 
+            i++;
+        }
+        else
+            grahamStack.erase(grahamStack.begin() + grahamStack.size() - 1);            
+    }
+
+    return grahamStack;
+}
+
+//Funktion berechnet die Konvexe Hülle einer gegebenen Menge von Punkten
+void convex_hull(std::vector<std::array<double, 2>>& points)
+{
+    std::sort(points.begin(), points.end(), sort_by_y); //Punkte werden nach Y-Wert sortiert
+
+    for(int i=1; i<points.size(); i++)
+    {
+        points[i][0] = {points[i][0] - points[0][0]};
+        points[i][1] = {points[i][1] - points[0][1]};
+    } 
+
+    std::sort(points.begin()+1, points.end(), sort_by_angle); //Punkte werden nach Winkel sortiert
+
+    for(int i=1; i<points.size(); i++)
+    {
+        points[i][0] = {points[i][0] + points[0][0]};
+        points[i][1] = {points[i][1] + points[0][1]};
+    }
+
+    std::vector<std::array<double, 2>> grahamStack;
+
+
+    grahamStack = grahamScan(points, grahamStack);
+    
+    write_points_in_file("hull.txt", grahamStack);
+
+    /*for(int i=0; i<grahamStack.size(); i++)
+    {
+        std::cout << "\t\tPunkt " << i+1 << " x: " << grahamStack[i][0];
+        std::cout << "\ty: " << grahamStack[i][1] << std::endl;
+    }*/
+}
+
 int main()
 {
-    read_point_from_file("convex-hull-points.txt");
+    std::vector<std::array<double, 2>> vec = read_points_from_file("points.txt");
+
+    convex_hull(vec);
 
     return 0;
 }
